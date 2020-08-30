014a1c0e33c7aac138444a8d2fed9192adaa6e94
Alberto Herrera <ahrfgb@google.com>
2020-08-30 17:47:06 +0000

    Expose library to extend the PCRs of tpm2 simulator
    Allow to directly extend the simulator's PCR.
    For example, it can be used to extend PCR0, which is used to indicate the device's boot mode.
    
    BUG=b:132144919
    TEST=hwsec.Login stops showing an error about the TPM not being initialized.
    
    Cq-Depend: chromium:2383214
    Change-Id: I1eaf2657df72e1de4b3eac6bf6cfee4640c29085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/tpm2/+/2383512
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Reviewed-by: Leo Lai <cylai@google.com>
    Tested-by: Alberto Herrera <ahrfgb@google.com>
    Commit-Queue: Alberto Herrera <ahrfgb@google.com>
    (cherry picked from commit 4897b83fe60dcac1fd035ac88b13af1b5e4e6105)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/tpm2/+/2510950
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>

--- tpm_manufacture.c
+++ tpm_manufacture.c
@@ -7,6 +7,7 @@
 
 #include "Global.h"
 #include "NV_fp.h"
+#include "PCR_Extend_fp.h"
 #include "Platform.h"
 #include "TPM_Types.h"
 
@@ -26,4 +27,17 @@ int tpm_manufactured(void) {
   }
 
   return 1;
+}
+
+TPM_RC extend_pcr(unsigned int pcr_index, const char *mode_digest) {
+  if (pcr_index >= IMPLEMENTATION_PCR)
+    return TPM_RC_FAILURE;
+
+  PCR_Extend_In pcr_in;
+  pcr_in.pcrHandle = HR_PCR + pcr_index;
+  pcr_in.digests.count = 1;
+  pcr_in.digests.digests[0].hashAlg = TPM_ALG_SHA256;
+  memcpy(pcr_in.digests.digests[0].digest.sha256, mode_digest,
+         SHA256_DIGEST_SIZE);
+  return TPM2_PCR_Extend(&pcr_in);
 }
